3223f5cb3a97c4a4f4ccbe6e66563775150c307c
Sebastian Gonzalez <seba.gnh@gmail.com>
2023-02-13 13:13:02 -0300

    Se utiliza .toBe y describe

--- README.md
+++ README.md
@@ -21,3 +21,17 @@ yarn test
     "test": "jest --watchAll"
   }
 }
+
+
+- Intellisense para jest 
+-  --dev es lo mismo que -D
+yarn add --dev @types/jest
+
+
+
+des + tab
+describe('first', () => { second })
+
+
+- Actualización
+Cambiar extensión de los archivos jest.config.js y babel.config.js a .cjs
--- tests/demo.test.js
+++ tests/demo.test.js
@@ -1,13 +1,41 @@
 
-
-test('Esta prueba no debe fallar', () => {
+// Prueba con error
+/* test('Esta prueba no debe fallar', () => {
   if ( 0 === 0 ) {
     throw new Error('No puede dividir entre cero');
   }
-})
+}) */
 
-test('Segunda prueba', () => {
+
+//Prueba que pasa sin error
+/* test('Segunda prueba', () => {
   if ( 0 === 1 ) {
     throw new Error('Prueba superada');
   }
+}) */
+
+
+
+// Prueba con descripción del componente en el que se t rabaja
+// Generalmente es un describe por cada prueba
+describe('Pruebs en <DemoComponent/>', () => { 
+  //Nombre de la prueba 
+  test('Tercer test', () => {
+    // 1- Inicialización
+      const mensaje_1 = 'Hola Mundo'
+      const mensaje_1B = 'Hola Mundo   '
+      
+      // 2- Estímulo
+      const mensaje_2 = mensaje_1.trim();
+
+    // 3- Observar el comportamiento...esperado, similar al if()
+    expect( mensaje_1 ).toBe( mensaje_2 );
+
+    /* expect( mensaje_1B ).toBe( mensaje_2 ); */
+  });
 })
+
+
+
+
+
